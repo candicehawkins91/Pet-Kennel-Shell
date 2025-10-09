e1f19a08d65165562b5e445b964b49917bb100e8
Candice Hawkins <candicehawkins91git config --global user.email candicehawkins91@gmail.com>
2025-10-08 22:27:10 -0500

    adding status method

---
+++ index-2.js
@@ -0,0 +1,19 @@
+function CreatePet(name, species, happiness,energy){
+    this.name = name
+    this.species = species
+    this.happiness = happiness
+    this.energy = energy
+
+    this.play = function(){
+        this.energy -=20; this.happiness +=10; 
+        return(`You played with ${this.name}! Happiness is now ${this.happiness} and energy is now ${this.energy}` )
+    }
+    this.feed = function(){
+        this.energy += 20; this.happiness +=20;
+        return(`You fed ${this.name} ! Happiness is now ${this.happiness} and energy is now ${this.energy}`)
+    }this.status = function
+
+}
+
+const pet1 = new CreatePet("Fluffy", "Dog", 50, 100);
+pet1.play();
