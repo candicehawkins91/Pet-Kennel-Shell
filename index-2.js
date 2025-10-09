fa0b71306e43b1937ec8cc0fa539395a225af4bd
Candice Hawkins <candicehawkins91git config --global user.email candicehawkins91@gmail.com>
2025-10-08 22:36:51 -0500

    finished day 2

--- index-2.js
+++ index-2.js
@@ -11,9 +11,17 @@ function CreatePet(name, species, happiness,energy){
     this.feed = function(){
         this.energy += 20; this.happiness +=20;
         return(`You fed ${this.name} ! Happiness is now ${this.happiness} and energy is now ${this.energy}`)
-    }this.status = function
+    }this.status = function(){
+        return(`Pet Name: ${this.name}
+            Species: ${this.species}
+            Happiness : ${this.happiness}
+            Energy: ${this.energy}   
+            `)
+    }
 
 }
 
 const pet1 = new CreatePet("Fluffy", "Dog", 50, 100);
 pet1.play();
+pet1.feed()
+pet1.status()
