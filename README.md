e2ea6ec32b37dd4bded2adbb73249a806d272862
Candice Hawkins <candicehawkins91git config --global user.email candicehawkins91@gmail.com>
2025-10-09 06:28:01 -0500

    orginal day 3

--- README.md
+++ README.md
@@ -39,7 +39,7 @@ let kennel = [];
 // Create more pets
 const pet4 = new CreatePet("Nemo", "Fish", 100, 25);
 const pet5 = new CreatePet("Smokey", "Cat", 85, 15);
-const pet6 = new CreatePet("BoJingles", "Horse", 50, 50);
+const pet6 = new CreatePet("BoJingles", "Horse", 50, 50); 
 const pet7 = new CreatePet("Tweetie", "Bird", 75, 0);
 const pet8 = new CreatePet("Queenie", "Dog", 25, 25);
 
--- index-3.js
+++ index-3.js
@@ -1,41 +1,40 @@
-function CreatePet(name, species, happiness, energy){
-    this.name = name
-    this.species = species
-    this.happiness = happiness
-    this.energy = energy
-    this.play = function () {
-      this.energy -= 20;
-      this.happiness += 10;
-      return `You played with ${this.name}! Happiness is now ${this.happiness} and energy is now ${this.energy}`;
-    };
-     this.feed = function(){
-        this.energy += 20; this.happiness +=20;
-        return(`You fed ${this.name} ! Happiness is now ${this.happiness} and energy is now ${this.energy}`)
-     }
-      this.status = function () {
-        return `Pet Name: ${this.name}
+function CreatePet(name, species, happiness, energy) {
+  this.name = name;
+  this.species = species;
+  this.happiness = happiness;
+  this.energy = energy;
+  this.play = function () {
+    this.energy -= 20;
+    this.happiness += 10;
+    return `You played with ${this.name}! Happiness is now ${this.happiness} and energy is now ${this.energy}`;
+  };
+  this.feed = function () {
+    this.energy += 20;
+    this.happiness += 20;
+    return `You fed ${this.name} ! Happiness is now ${this.happiness} and energy is now ${this.energy}`;
+  };
+  this.status = function () {
+    return `Pet Name: ${this.name}
             Species: ${this.species}
             Happiness : ${this.happiness}
             Energy: ${this.energy}   
             `;
-      };
-
+  };
 }
-    const pet1 = new CreatePet("Fluffy", "Dog", 95, 25 );
-    const pet2 = new CreatePet("Taz","Dog", 45, 10);
-    const pet3 = new CreatePet("Nyla","Cat", 65, 35);
+const pet1 = new CreatePet("Fluffy", "Dog", 95, 25);
+const pet2 = new CreatePet("Taz", "Dog", 45, 10);
+const pet3 = new CreatePet("Nyla", "Cat", 65, 35);
 
-    let kennel = []
+let kennel = [];
 
-    const pet4 = new CreatePet("Nemo", "Fish", 100, 25)
-    const pet5 = new CreatePet("Smokey", "Cat", 85, 15)
-    const pet6 = new CreatePet("BoJingles", "Horse", 50, 50)
-    const pet7 = new CreatePet("Tweetie", "Bird", 75, 0)
-    const pet8 = new CreatePet("Queenie", "Dog", 25, 25)
+const pet4 = new CreatePet("Nemo", "Fish", 100, 25);
+const pet5 = new CreatePet("Smokey", "Cat", 85, 15);
+const pet6 = new CreatePet("BoJingles", "Horse", 50, 50);
+const pet7 = new CreatePet("Tweetie", "Bird", 75, 0);
+const pet8 = new CreatePet("Queenie", "Dog", 25, 25);
 
-    kennel.push = {pet4, pet5, pet6, pet7, pet8};
-console.log(kennel.push())
-    
-kennel.push(
+kennel.push = { pet4, pet5, pet6, pet7, pet8 };
+console.log(kennel.push());
 
-)
+kennel.push();
+
